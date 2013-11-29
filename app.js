eae82ebdda6637367aec204e436fa49220751b3d
Robin Hu <robinfhu@gmail.com>
2013-11-29 11:50:51 -0500

    Adding directives, controller. Got pie chart to show.

--- app.js
+++ app.js
@@ -1,34 +1,72 @@
 var nvApp = angular.module('nvApp',[]);
 
+/*
+Controller for the application.
+*/
 nvApp.controller('PieChartController', function($scope) {
+
+	$scope.newValue = 2;
+	$scope.title = "Pie Chart Widget";
 	$scope.pieData = [
 	    {
 	      key: "One",
 	      y: 5
-	    },
-	    {
-	      key: "Two",
-	      y: 2
-	    },
-	    {
-	      key: "Three",
-	      y: 9
-	    },
-	    {
-	      key: "Four",
-	      y: 7
-	    },
-	    {
-	      key: "Five",
-	      y: 4
-	    },
-	    {
-	      key: "Six",
-	      y: 3
-	    },
-	    {
-	      key: "Seven",
-	      y: .5
 	    }
 	  ];
+
+	$scope.count = 2;
+	$scope.addData = function add(dataValue) {
+		console.log('adding: ' + dataValue);
+		$scope.pieData.push({
+			key: 'Data ' + $scope.count,
+			value: dataValue
+		});
+
+		$scope.count++;
+	};
+});
+
+//Create a directive for <div nv-pie-chart>
+nvApp.directive('nvPieChart', function(){
+	return {
+		restrict: 'A',
+		template: '<svg></svg>',
+		scope: {
+			chartData: '='
+		},
+		link: function(scope,element,attrs) {
+			var svgElem = element.find('svg')[0];
+			//Call nvd3 to add the chart.
+			nv.addGraph(function() {
+			    var width = attrs.chartWidth,
+			        height = attrs.chartHeight;
+
+			    var chart = nv.models.pieChart()
+			        .x(function(d) { return d.key })
+			        .y(function(d) { return d.y })
+			        .color(d3.scale.category10().range())
+			        .width(width)
+			        .height(height);
+
+			      d3.select(svgElem)
+			          .datum(scope.chartData)
+			        .transition().duration(1200)
+			          .attr('width', width)
+			          .attr('height', height)
+			          .call(chart);
+
+			    chart.dispatch.on('stateChange', function(e) { nv.log('New State:', JSON.stringify(e)); });
+
+			    return chart;
+			});
+		}
+	};
+});
+
+
+//Directive that sets focus for an input box.
+nvApp.directive('startFocus', function() {
+	return function(scope,elem,attrs) {
+		elem[0].focus();
+	};
 });
