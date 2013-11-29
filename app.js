c78ad74bab40979b33614036a6bb82e64a2cdb98
Robin Hu <robinfhu@gmail.com>
2013-11-29 12:28:05 -0500

    Fixed all errors. Added second pie component.

--- app.js
+++ app.js
@@ -14,12 +14,19 @@ nvApp.controller('PieChartController', function($scope) {
 	    }
 	  ];
 
+	$scope.pieRandomData = [];
+
 	$scope.count = 2;
 	$scope.addData = function add(dataValue) {
 		console.log('adding: ' + dataValue);
 		$scope.pieData.push({
 			key: 'Data ' + $scope.count,
 			y: dataValue
+		});
+
+		$scope.pieRandomData.push({
+			key: 'Data ' + $scope.count,
+			y: Math.random()
 		});
 
 		$scope.count++;
@@ -44,40 +51,40 @@ nvApp.directive('nvPieChart', function(){
 			chartData: '='
 		},
 		link: function(scope,element,attrs) {
+			element.css('height','300px');
 			var svgElem = element.find('svg')[0];
-			var chart;
 			scope.$watch('chartData', function(newVal,oldVal) {
-				if (chart) {
-					d3.select(svgElem)
-						.datum(newVal)
-						.call(chart);
-				}
+				newChart(newVal);
 			},true);
 			
+			function newChart(data) {
+				//Call nvd3 to add the chart.
+				nv.addGraph(function() {
+				    var width = attrs.chartWidth,
+				        height = attrs.chartHeight;
+
+				    var chart = nv.models.pieChart()
+				        .x(function(d) { return d.key })
+				        .y(function(d) { return d.y })
+				        .color(d3.scale.category10().range())
+				        .width(width)
+				        .height(height);
+
+				      d3.select(svgElem)
+				          .datum(data)
+				        .transition().duration(1200)
+				          .attr('width', width)
+				          .attr('height', height)
+				          .call(chart);
+
+				    chart.dispatch.on('stateChange', function(e) { nv.log('New State:', JSON.stringify(e)); });
+
+				    return chart;
+				});
+			}
+
+			newChart(scope.chartData);
 			
-			//Call nvd3 to add the chart.
-			nv.addGraph(function() {
-			    var width = attrs.chartWidth,
-			        height = attrs.chartHeight;
-
-			    chart = nv.models.pieChart()
-			        .x(function(d) { return d.key })
-			        .y(function(d) { return d.y })
-			        .color(d3.scale.category10().range())
-			        .width(width)
-			        .height(height);
-
-			      d3.select(svgElem)
-			          .datum(scope.chartData)
-			        .transition().duration(1200)
-			          .attr('width', width)
-			          .attr('height', height)
-			          .call(chart);
-
-			    chart.dispatch.on('stateChange', function(e) { nv.log('New State:', JSON.stringify(e)); });
-
-			    return chart;
-			});
 		}
 	};
 });
